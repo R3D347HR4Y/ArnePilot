7430c352b0b779104b94e952699a29d5368c19b3
ReFil <31960031+ReFil@users.noreply.github.com>
2021-05-15 16:32:53 +0100

    Update interface.py

--- selfdrive/car/ocelot/interface.py
+++ selfdrive/car/ocelot/interface.py
@@ -112,11 +112,10 @@ class CarInterface(CarInterfaceBase):
     ret.engineRPM = self.CS.engineRPM
 
     for button in self.CS.buttonStates:
-      if self.CS.buttonStates[button] != self.buttonStatesPrev[button]:
-        be = car.CarState.ButtonEvent.new_message()
-        be.type = button
-        be.pressed = self.CS.buttonStates[button]
-        buttonEvents.append(be)
+      be = car.CarState.ButtonEvent.new_message()
+      be.type = button
+      be.pressed = self.CS.buttonStates[button]
+      buttonEvents.append(be)
 
 
 
