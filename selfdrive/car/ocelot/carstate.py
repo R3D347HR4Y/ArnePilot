5bc121014c26cea8bc38bfb72e14a0fbb8b9c8d5
ReFil <31960031+ReFil@users.noreply.github.com>
2021-05-16 13:22:42 +0100

    Update carstate.py

--- selfdrive/car/ocelot/carstate.py
+++ selfdrive/car/ocelot/carstate.py
@@ -84,10 +84,7 @@ class CarState(CarStateBase):
     #    ret.cruiseState.enabled = not ret.cruiseState.enabled
     ret.cruiseState.enabled = bool(cp.vl["HIM_CTRLS"]['SET_BTN'])
 
-    if not ret.cruiseState.enabled:
-      ret.cruiseState.speed = 5
-      if ret.standstill:
-        ret.cruiseState.speed = 10*CV.MPH_TO_MS
+    ret.cruiseState.speed = 10*CV.MPH_TO_MS
 
     if self.buttonStates["accelCruise"] and not self.oldButtonStates["accelCruise"]:
       ret.cruiseState.speed = ret.cruiseState.speed + 5*CV.MPH_TO_MS
