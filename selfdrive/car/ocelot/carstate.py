149ca4957cb2276387b4911373cf03ff0853ff81
seb43654 <sebsmith3@gmail.com>
2021-05-17 20:18:26 +0100

    Update carstate.py
    fix colon syntax

--- selfdrive/car/ocelot/carstate.py
+++ selfdrive/car/ocelot/carstate.py
@@ -91,10 +91,10 @@ class CarState(CarStateBase):
     if bool(self.buttonStates["setCruise"]) and not self.oldEnabled:
       print("attempt enable")
       self.enabled = not self.enabled
-      if self.enabled
-        self.setSpeed = (int_rnd((ret.vEgo * CV.MS_TO_MPH)/5) * 5)
-        if ret.standstill:
-          self.setSpeed = 10
+      if self.enabled:
+          self.setSpeed = (int_rnd((ret.vEgo * CV.MS_TO_MPH)/5) * 5)
+          if ret.standstill:
+            self.setSpeed = 10
 
     if bool(self.buttonStates["accelCruise"]) and not self.oldSpeedUp:
       print("speedup")
