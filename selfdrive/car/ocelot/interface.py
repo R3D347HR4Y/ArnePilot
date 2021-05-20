dbc3eab93e5ed334bdba7399cd31e5da52f0293b
seb43654 <sebsmith3@gmail.com>
2021-05-20 15:12:15 +0100

    update tuning and radar

--- selfdrive/car/ocelot/interface.py
+++ selfdrive/car/ocelot/interface.py
@@ -31,17 +31,17 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.init('pid')
     ret.safetyModel = car.CarParams.SafetyModel.allOutput
 
-    ret.steerActuatorDelay = 0.10
+    ret.steerActuatorDelay = 0.09
     ret.steerLimitTimer = 0.4
 
     if candidate == CAR.SMART_ROADSTER_COUPE:
         ret.lateralTuning.init('pid')
         ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.008]]
-        ret.lateralTuning.pid.kf = 0.   # full torque for 20 deg at 80mph means 0.00007818594
+        ret.lateralTuning.pid.kf = 0.   #was 0.00007818594
         ret.safetyParam = 100
         ret.wheelbase = 2.36
-        ret.steerRatio = 15
+        ret.steerRatio = 14.8
         tire_stiffness_factor = 0.444
         ret.mass = 810 + STD_CARGO_KG
         ret.steerRateCost = 1.
