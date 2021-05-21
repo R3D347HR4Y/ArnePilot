d0068b167bca3042af121b7082dbacfacf17859c
seb43654 <sebsmith3@gmail.com>
2021-05-21 16:32:27 +0100

    Update radar_interface.py
    undo negative sign

--- selfdrive/car/ocelot/radar_interface.py
+++ selfdrive/car/ocelot/radar_interface.py
@@ -55,7 +55,7 @@ class RadarInterface(RadarInterfaceBase):
           self.track_id += 1
         self.pts[ii].dRel = cpt['CAN_DET_RANGE']  # from front of car
         self.pts[ii].yRel = cpt['CAN_DET_RANGE'] * -cpt['CAN_DET_AZIMUTH']   # in car frame's y axis, left is positive
-        self.pts[ii].vRel = -cpt['CAN_DET_RANGE_RATE']
+        self.pts[ii].vRel = cpt['CAN_DET_RANGE_RATE']
         self.pts[ii].aRel = float('nan')
         self.pts[ii].yvRel = float('nan')
         self.pts[ii].measured = True
